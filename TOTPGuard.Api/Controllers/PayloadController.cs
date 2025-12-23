fd63ba65fb1e19890c04b9947e479dd334973395
Mohsen Akbari-Shahpar <mohsen-akbari@outlook.com>
2025-12-23 14:34:19 +0330

    Used safe base64 instead of hex strings

--- TOTPGuard.Api/Controllers/PayloadController.cs
+++ TOTPGuard.Api/Controllers/PayloadController.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using OtpNet;
 using TOTPGuard.Api.Options;
 using TOTPGuard.Api.Providers;
 using TOTPGuard.Shared;
@@ -16,18 +19,20 @@ public class PayloadController(
     [HttpPost]
     public async Task<string> EncodeAsync(PayloadEncodingRequest payload, CancellationToken cancellationToken)
     {
-        return await symmetricCryptographyProvider.EncryptAsync(payload, symmetricCryptographyOptions.Key, cancellationToken);
+        return Base64UrlTextEncoder.Encode(await symmetricCryptographyProvider.EncryptAsync(payload.ToByteArray(), symmetricCryptographyOptions.Key, cancellationToken));
     }
 
     [HttpGet]
     public async Task<IActionResult> GenerateOtpAsync(string encodedPayload,
         CancellationToken cancellationToken)
     {
-        var payload = await symmetricCryptographyProvider.DecryptAsync<PayloadEncodingRequest>(
-            encodedPayload,
+        var payloadBytes = await symmetricCryptographyProvider.DecryptAsync(
+            Base64UrlTextEncoder.Decode(encodedPayload),
             symmetricCryptographyOptions.Key,
             cancellationToken
         );
+
+        var payload = PayloadEncodingRequest.FromByteArray(payloadBytes);
 
         if (DateTime.UtcNow > payload.ExpiresAtUtc)
             return NotFound();
--- TOTPGuard.Shared/PayloadEncodingRequest.cs
+++ TOTPGuard.Shared/PayloadEncodingRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace TOTPGuard.Shared;
+﻿using System.Text;
+
+namespace TOTPGuard.Shared;
 
 public class PayloadEncodingRequest
 {
@@ -6,4 +8,36 @@ public class PayloadEncodingRequest
     public string Secret { get; set; } = string.Empty;
     public int Step { get; set; } = 30;
     public DateTime ExpiresAtUtc { get; set; }
+
+    public byte[] ToByteArray()
+    {
+        var bytes = new List<byte>();
+
+        bytes.AddRange(BitConverter.GetBytes(Step)); // 4 bytes
+        bytes.AddRange(BitConverter.GetBytes(ExpiresAtUtc.ToBinary())); // 8 bytes
+
+        var labelBytes = Encoding.UTF8.GetBytes(Label);
+        bytes.AddRange(BitConverter.GetBytes(labelBytes.Length)); // 4 bytes
+        bytes.AddRange(labelBytes); // dynamic length
+
+        var secretBytes= Encoding.UTF8.GetBytes(Secret);
+        bytes.AddRange(BitConverter.GetBytes(secretBytes.Length)); // 4 bytes
+        bytes.AddRange(secretBytes); // dynamic length
+
+
+        return bytes.ToArray();
+    }
+
+    public static PayloadEncodingRequest FromByteArray(byte[] data)
+    {
+        var labelLength = BitConverter.ToInt32(data, 12);
+        var secretLength = BitConverter.ToInt32(data, 16 + labelLength);
+        return new PayloadEncodingRequest
+        {
+            Label = Encoding.UTF8.GetString(data, 16, labelLength),
+            Secret = Encoding.UTF8.GetString(data, 20 + labelLength, secretLength),
+            Step = BitConverter.ToInt32(data, 0),
+            ExpiresAtUtc = DateTime.FromBinary(BitConverter.ToInt64(data, 4)),
+        };
+    }
 }
